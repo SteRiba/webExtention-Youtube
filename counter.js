94138982e418c4f29ff99fc7913a4379217fa9e2
Stefano <sribaldone@gmail.com>
2024-11-16 22:12:18 +0100

    added types; testing counter

--- counter.js
+++ counter.js
@@ -1,9 +1,21 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  let counter = 0;
+  const setCounter = async (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+    if (count > 0) {
+      const tab = await chrome.tabs.query({ active: true });
+      chrome.scripting.executeScript({
+        target: {
+          tabId: tab[0].id,
+        },
+        func: () => {
+          // document.body.style.backgroundColor = "red";
+          console.log("aaaaaa");
+        },
+      });
+    }
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
 }
